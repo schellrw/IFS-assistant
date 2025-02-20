45018ce4b0f9352a978cc32ebf88e4565fb3e621
schellrw <schell.rw@gmail.com>
2025-02-19 21:36:44 -0600

    adding components

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from ifs_assistant import IFSSystem, Part, Relationship, Journal
+from ifs import IFSSystem, Part, Relationship, Journal
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
